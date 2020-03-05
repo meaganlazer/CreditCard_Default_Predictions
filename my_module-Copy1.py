0ffc0e8ef36d64d6ffc73667bc947bcff0152c16
Meagan Rossi <meaganrossi@gmail.com>
2020-03-05 10:50:19 -0500

    new

---
+++ my_module-Copy1.py
@@ -0,0 +1,21 @@
+print("I am being executed!")
+
+import pandas as pd
+from math import exp
+from scipy.stats import norm
+from scipy import stats
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+import itertools
+import warnings
+import statsmodels.api as sm
+
+some_list = ["a", 1, None]
+
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
